f0ad9d3e8b845a7afc377bc2cc3f34fdec00968c
Didi <reinaldi.mukti@flash-coffee.com>
2023-10-09 10:18:48 +0700

    feat: define every possibility in main function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as readline from 'readline';
 import * as fs from 'fs';
+import { Warehouse } from './modules/warehouse';
 
 const args = process.argv;
 
+let warehouse: Warehouse = new Warehouse(0);
 let rl: readline.Interface;
 
 if (args[2] !== undefined) {
@@ -27,18 +29,25 @@ rl.on('line', function (line) {
   const input = line.split(' ');
   switch (input[0].trim()) {
     case 'create_warehouse_rack':
+      createWarehouse(Number(input[1]));
       break;
     case 'rack':
+      addRack(input[1], input[2]);
       break;
     case 'rack_out':
+      removeRack(Number(input[1]));
       break;
     case 'status':
+      getStatusRack(warehouse);
       break;
     case 'sku_numbers_for_product_with_exp_date':
+      getSkuProductByExpDate(input[1]);
       break;
     case 'slot_numbers_for_product_with_exp_date':
+      getSlotNumberProductByExpDate(input[1]);
       break;
     case 'slot_number_for_sku_number':
+      findSlotNumberProductBySku(input[1]);
       break;
     case 'exit':
       process.exit(0);
@@ -50,3 +59,19 @@ rl.on('line', function (line) {
 }).on('close', function () {
   process.exit(0);
 });
+
+function createWarehouse(rackLength: number) {
+  warehouse = new Warehouse(rackLength);
+}
+
+function getStatusRack(warehouse: Warehouse) {}
+
+function addRack(sku: string, expDate: string) {}
+
+function removeRack(rackNo: number) {}
+
+function getSkuProductByExpDate(expDate: string) {}
+
+function getSlotNumberProductByExpDate(expDate: string) {}
+
+function findSlotNumberProductBySku(sku: string) {}
